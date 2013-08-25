a0c0c6dfb9d97b2ec7e34ecf786ec418220cadac
aditya <aswani.aditya@gmail.com>
2013-08-25 20:36:40 +0530

    blackjack
    Change-Id: I19e501c0c907676dd11574739ba905ef0d72ad59
    Signed-off-by: aditya <aswani.aditya@gmail.com>

---
+++ SWCompetency/src/blackjack/SolutionBlackjack.java
@@ -0,0 +1,13 @@
+package blackjack;
+
+public class SolutionBlackjack {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("black jack solution");
+	}
+
+}
