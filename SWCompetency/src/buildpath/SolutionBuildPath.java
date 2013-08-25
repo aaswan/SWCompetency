27254e67bd4785347b9fea3f64ce3081b1a50543
aditya <aswani.aditya@gmail.com>
2013-08-25 20:32:38 +0530

    added build path
    Change-Id: I7202ee3a50ea57423318bf529169f9434b3bf415
    Signed-off-by: aditya <aswani.aditya@gmail.com>

---
+++ SWCompetency/src/buildpath/SolutionBuildPath.java
@@ -0,0 +1,13 @@
+package buildpath;
+
+public class SolutionBuildPath {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("build path");
+	}
+
+}
